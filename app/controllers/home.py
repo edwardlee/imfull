a6891c4ece8f03f32f889a0b5dcd5991e19cda23
Talal Ghafoor <talalghafoor@gmail.com>
2020-10-24 14:38:44 -0500

    Doing calculator

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -1,8 +1,21 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, render_template
-
+import requests
 blueprint = Blueprint('home', __name__)
+
+
+
 
 @blueprint.route('/')
 def index():
-    return render_template('home/index.html')
+    # Search GitHub's repositories for requests
+    response = requests.get(
+        'https://api.github.com/search/repositories',
+        params={'q': 'requests+language:python'},
+    )
+
+    # Inspect some attributes of the `requests` repository
+    json_response = response.json()
+    repository = json_response['items'][0]
+
+    return render_template('home/index.html', helpfuldata=repository["description"])
